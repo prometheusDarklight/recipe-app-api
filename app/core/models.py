96c2f0e35db440fbfdab52ff11e1454a75bc3794
prometheusDarklight <ceo@droneultraimages.com>
2024-03-08 21:20:54 +0000

    Added custome User  model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,32 @@
-from django.db import models  # noqa
+"""
+Database Models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
 
-# Create your models here.
+)
+
+
+class UserManager(BaseUserManager):
+    """manager for users."""
+    def create_user(self, email, password=None, **extra_field):
+        """create save and return a new user."""
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """ User in System """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
